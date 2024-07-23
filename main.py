f3c3d38a44173e1c703461a921454d41e32a0e01
Monika Etrych <monika.etrych@gmail.com>
2024-07-23 11:24:18 +0200

    select defined hours of current weather

---
+++ main.py
@@ -0,0 +1,11 @@
+from select_history_weather import select_history_weather
+from select_current_weather import select_current_weather
+
+
+def main():
+    select_current_weather(48)
+    select_history_weather()
+
+
+if __name__ == "__main__":
+    main()
